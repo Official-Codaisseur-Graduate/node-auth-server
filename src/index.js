e139e4384281a3e980108675840dac44cf5e4c86
Billy Vlachos <williamcaminiti@live.com>
2019-07-22 13:11:59 +0200

    Add a redis adapter for tockens and sessions

--- src/index.js
+++ src/index.js
@@ -7,13 +7,27 @@ assert(process.env.HEROKU_APP_NAME, 'process.env.HEROKU_APP_NAME missing');
 assert(process.env.PORT, 'process.env.PORT missing');
 assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addons:create securekey`');
 assert.equal(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
+assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`');
 
 const jwks = require('./jwks.json');
 
+// require the redis adapter factory/class
+const RedisAdapter = require('./redis_adapter');
+
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
-  clients: [{ client_id: 'foo', client_secret: 'bar', redirect_uris: ['http://lvh.me/cb'] }],
+  // configure Provider to use the adapter
+  adapter: RedisAdapter,
+  clients: [
+    // reconfigured the foo client for the purpose of showing the adapter working
+    {
+      client_id: 'foo',
+      redirect_uris: ['https://example.com'],
+      response_types: ['id_token'],
+      grant_types: ['implicit'],
+      token_endpoint_auth_method: 'none',
+    },
+  ],
   jwks,
-  // enable some of the feature, see the oidc-provider readme for more
   formats: {
     AccessToken: 'jwt',
   },
