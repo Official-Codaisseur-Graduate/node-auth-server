ff8705b85ab9c80375193a72772bbcc07062463a
Billy Vlachos <williamcaminiti@live.com>
2019-07-22 19:48:08 +0200

    Add secret key to config file

--- src/config.js
+++ src/config.js
@@ -9,11 +9,10 @@ module.exports = ({
         ISSUER: (HEROKU_APP_NAME !== 'localhost' ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : `http://localhost:${PORT}`)
     },
     SECURITY: {
-        SECURE_KEY: process.env.SECURE_KEY,
+        SECURE_KEY: process.env.SECURE_KEY || '1C281FFC0280B7A04F4237A397B55542F2D8B9A0365FBB6D8B9D9CB899C69687,E901DF3944E93756738090BA2362419CF609155FA8AC544FDE2400618F181D68',
 
     },
     STORES: {
-        REDIS_URL: assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`');
-
+        REDIS_URL: assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`')
     }
 });
