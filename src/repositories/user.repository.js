e0566497bf5369c42e0902074bdef9c35f58f535
Billy Vlachos <williamcaminiti@live.com>
2019-07-23 12:57:05 +0200

    Create use repository

---
+++ src/repositories/user.repository.js
@@ -0,0 +1,8 @@
+/**
+ * THE USER REPOSITORY
+ * Will be used to retrieve users and their claims and will be converting these into a USER model
+ * This user model will only be used by the ADMIN who will be managing users.
+ */
+module.exports = {
+
+}
