35b29dfabb2af41a973afad0580ce207c9935827
Billy Vlachos <williamcaminiti@live.com>
2019-07-23 13:21:44 +0200

    Define basic db schema

---
+++ src/entities/claim.entity.js
@@ -0,0 +1,22 @@
+// Import required modules
+const Sequelize = require('sequelize');
+const db = require('./../db');
+
+// Define a claim entity
+const Claim = db.define('claim', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    value: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+        tableName: 'claims',
+        timestamps: false
+    }
+);
+
+// Export the claim entity model
+module.exports = Claim;
---
+++ src/entities/user.entity.js
@@ -0,0 +1,45 @@
+// Import required modules
+const Sequelize = require('sequelize');
+const db = require('./../db');
+
+// Import linked entities
+const Claim = require('./claim.entity');
+
+// Define a user model
+const User = db.define('user', {
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    emailConfirmed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    },
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    gender: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    dateOfBirth: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    }
+}, {
+        tableName: 'users',
+        timestamps: false
+    }
+);
+
+// Define relationships
+User.hasMany(Claim);
+Claim.belongsTo(User);
+
+// Export the user entity model
+module.exports = User;
