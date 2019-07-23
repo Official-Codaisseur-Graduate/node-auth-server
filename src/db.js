f021ec58707c76126d8936080c6012186baa72ee
Billy Vlachos <williamcaminiti@live.com>
2019-07-23 13:16:48 +0200

    Create database

---
+++ src/db.js
@@ -0,0 +1,18 @@
+// Import required models
+const Sequelize = require('sequelize');
+const config = require('./config');
+const databaseUrl = config.connectionString;
+
+// Create a new instance of sequelize
+const sequelize = new Sequelize(databaseUrl);
+
+// Sync the data (create schemas)
+sequelize.sync({ force: true })
+    .then(r => {
+        // Log success
+        console.log('Database schema created!');
+    })
+    .catch(console.log);
+
+// Export the sequelize instance
+module.exports = sequelize;
